0f516432ce9cbeb2a16665882cf388c3bc196830
eduardosidney <aleduardooliveira@gmail.com>
2022-08-24 12:01:45 -0300

    create wazuhagentautoinstall.sh

---
+++ wazuhagentautoinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/env bash
+
+#Import the GPG key:
+
+rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
+
+
+#Add the repository:
+
+cat > /etc/yum.repos.d/wazuh.repo << EOF
+[wazuh]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=EL-\$releasever - Wazuh
+baseurl=https://packages.wazuh.com/4.x/yum/
+protect=1
+EOF
+
+
+
+#Deploy a Wazuh agent
+
+WAZUH_MANAGER="10.0.0.2"
+
+yum install wazuh-agent -y
+
+
+
+systemctl daemon-reload && systemctl enable wazuh-agent && systemctl start wazuh-agent
+
+
+
+systemctl restart wazuh-agent.service
+
+systemctl status wazuh-agent.service
