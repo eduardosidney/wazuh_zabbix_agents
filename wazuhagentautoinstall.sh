42f9e5813b868b42d2f20d2c5cca29e2965dbc51
eduardosidney <aleduardooliveira@gmail.com>
2022-08-26 10:24:28 -0300

    modified wazu

--- wazuhagentautoinstall.sh
+++ wazuhagentautoinstall.sh
@@ -1,11 +1,15 @@
 #!/bin/env bash
 
-#Import the GPG key:
+################################################
+#  			Download package				   #
+################################################
 
 rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
 
 
-#Add the repository:
+################################################
+#   			Add repository 				   #
+################################################
 
 cat > /etc/yum.repos.d/wazuh.repo << EOF
 [wazuh]
@@ -17,18 +21,31 @@ baseurl=https://packages.wazuh.com/4.x/yum/
 protect=1
 EOF
 
+################################################
+#   		  Deploy a Wazuh Agent             #
+################################################
 
-
-#Deploy a Wazuh agent
 
 WAZUH_MANAGER="10.0.0.2"
+
+
+################################################
+#  			 Install wazuh-agent               #
+################################################
 
 yum install wazuh-agent -y
 
 
+################################################
+#            Restart, enable and start         #
+################################################
+
 
 systemctl daemon-reload && systemctl enable wazuh-agent && systemctl start wazuh-agent
 
+################################################
+#   Substitui MANAGER_IP pelo ip do servidor   #
+################################################
 
 sed -i '10 s/'MANAGER_IP'/'132.226.255.149'/' /var/ossec/etc/ossec.conf
 systemctl restart wazuh-agent
