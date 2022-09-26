4d4e8736c87d83d55e67685cd90dae3d0d98db1d
Eduardo <80078223+eduardosidney@users.noreply.github.com>
2022-09-26 10:53:37 -0300

    remove ip

--- autorunzabbixagent.sh
+++ autorunzabbixagent.sh
@@ -23,8 +23,8 @@ systemctl enable zabbix-agent
 ################################################
 
 
-sed -i '117 s/'Server=127.0.0.1'/'Server=168.138.134.255'/' /etc/zabbix/zabbix_agentd.conf
-sed -i '170 s/'ServerActive=127.0.0.1'/'ServerActive=168.138.134.255'/' /etc/zabbix/zabbix_agentd.conf
+sed -i '117 s/'Server=127.0.0.1'/'Server=IPDESEJADO'/' /etc/zabbix/zabbix_agentd.conf
+sed -i '170 s/'ServerActive=127.0.0.1'/'ServerActive=IPDESEJADO'/' /etc/zabbix/zabbix_agentd.conf
 #sed -i '181 s/'Zabbix server'/'TSAGLBSRV030'/' /etc/zabbix/zabbix_agentd.conf
 
 ################################################
